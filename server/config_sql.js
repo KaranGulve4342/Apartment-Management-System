e8279f2d474d2c769983ec49e239b5069f59eab5
KaranGulve4342 <karangulve4342@gmail.com>
2024-04-15 22:45:16 +0530

    All added

---
+++ server/config_sql.js
@@ -0,0 +1,9 @@
+const config = {
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "karan4342",
+  database: process.env.DB_DATABASE || "dbms_project",
+  insecureAuth: true,
+};
+
+module.exports = config;
